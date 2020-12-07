a77037fc788d7a26579c8567d442b8c322ec863b
julianmangut <julianmangut@gmail.com>
2020-12-07 17:45:33 +0100

    style: Added quote.

--- portfolio-website/src/header/Header.js
+++ portfolio-website/src/header/Header.js
@@ -1,16 +1,18 @@
 import './Header.css'
 
 function Header() {
-
     return (
-
         <div id="principal-img">
             <img id="profile-img" src="./profile-img.jpg" />
-            <h2 id="principal-text">Julián Cuéllar Mangut</h2>
+            <div id="principal-text">
+                <h2>Julián Cuéllar Mangut</h2>
+                <blockquote>
+                    "Tell me and I forget.  Teach me and I remember.  Involve me and I learn."
+                    <cite>Benjamin Franklin</cite>
+                </blockquote>
+            </div>
         </div>
-        
     )
-
 }
 
 export default Header;
