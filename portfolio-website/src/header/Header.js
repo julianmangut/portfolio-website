ebe247e3e88d31b1e19fc70bb57b7a01376ca1c7
julianmangut <julianmangut@gmail.com>
2020-12-07 15:28:02 +0100

    feature: Added image for header and center text.

---
+++ portfolio-website/src/header/Header.js
@@ -0,0 +1,16 @@
+import './Header.css'
+
+function Header() {
+
+    return (
+
+        <div id="principal-img">
+            <img id="profile-img" src="./profile-img.jpg" />
+            <h2 id="principal-text">Julián Cuéllar Mangut</h2>
+        </div>
+        
+    )
+
+}
+
+export default Header;
